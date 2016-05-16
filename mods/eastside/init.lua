8a8efbb7acf2db197f7b0464d8326c25d36dba38
HybridDog <ovvv@web.de>
2016-05-16 14:19:08 +0200

    update eastside mapgen code

--- mods/eastside/init.lua
+++ mods/eastside/init.lua
@@ -7,7 +7,6 @@ local function inform(msg)
 end
 
 local SIZE = minetest.setting_get("generator_size") or 1000
-local chunksize = minetest.setting_get("chunksize") or 5
 
 local CAVESIZE = 3000
 
@@ -28,9 +27,10 @@ end
 --local recursion_depth = math.ceil(math.abs(SIZE)/10)
 
 local function do_ws_func(depth, a, x)
+	local n = math.pi * x
 	local y = 0
 	for k=1,depth do
-		y = y + math.sin(math.pi * k^a * x)/(k^a)
+		y = y + math.sin(n * k^a)/(k^a)
 	end
 	if math.floor(math.abs(x))%2 == 1 then
 		y = -y
@@ -38,33 +38,37 @@ local function do_ws_func(depth, a, x)
 	return y/math.pi
 end
 
-local ws_lists = {}
-local function get_ws_list(a, x, m, depth)
-	if not depth then
-		depth = ssize
-	end
-	local v = ws_lists[a]
+local ws_values = {}
+local function get_ws_value(depth, a, x)
+	local v = ws_values[depth]
 	if v then
-		v = v[m]
+		v = v[a]
 		if v then
 			v = v[x]
 			if v then
 				return v
 			end
 		else
-			ws_lists[a][m] = {}
+			v = ws_values[depth]
+			v[a] = {}
+			setmetatable(v[a], {__mode = "kv"})
 		end
 	else
-		ws_lists[a] = {[m]={}}
+		ws_values[depth] = {[a] = {}}
 	end
-	v = {}
-	for x=x,x + (chunksize*16 - 1) do
-		local y = do_ws_func(depth, a, x / m)
-		v[x] = y
-	end
-	ws_lists[a][m][x] = v
+	v = do_ws_func(depth, a, x)
+	ws_values[depth][a][x] = v
 	return v
 end
+
+local function get_vs_list(from, to, depth, a, m)
+	local list = {}
+	for i = from, to do
+		list[i] = get_ws_value(depth, a, i / m)
+	end
+	return list
+end
+
 
 local get_distance = math.hypot
 
@@ -111,29 +115,32 @@ table.insert(minetest.registered_on_generateds, 1, function(minp, maxp, seed)
 		data[p_pos] = c_air
 	end
 
+	local c1listx = get_vs_list(minp2.x, maxp.x, CAVESIZE, 2, CAVESIZE * 20)
+	local c2listx = get_vs_list(minp2.x, maxp.x, CAVESIZE, 6, CAVESIZE * 20)
+	local hlistx = get_vs_list(minp2.x, maxp.x, ssize, 3, SIZE * 20)
 
-	local heightx = get_ws_list(3, minp.x, SIZE * 20)
-	local heightz = get_ws_list(5, minp.z, SIZE * 20)
+	local c1listy = get_vs_list(minp.y, maxp.y, CAVESIZE, 5, CAVESIZE * 10)
+	local c2listy = get_vs_list(minp.y, maxp.y, CAVESIZE, 3, CAVESIZE * 10)
 
-	local cave1x = get_ws_list(2, minp.x, CAVESIZE * 20, CAVESIZE)
-	local cave1y = get_ws_list(5, minp.y, CAVESIZE * 10, CAVESIZE)
-	local cave1z = get_ws_list(4, minp.z, CAVESIZE * 10, CAVESIZE)
+	local c1listz = get_vs_list(minp.z, maxp.z, CAVESIZE, 4, CAVESIZE * 10)
+	local c2listz = get_vs_list(minp.z, maxp.z, CAVESIZE, 2.5, CAVESIZE * 10)
+	local hlistz = get_vs_list(minp.z, maxp.z, ssize, 5, SIZE * 20)
 
-	local cave2x = get_ws_list(6, minp.x, CAVESIZE * 20, CAVESIZE)
-	local cave2y = get_ws_list(3, minp.y, CAVESIZE * 10, CAVESIZE)
-	local cave2z = get_ws_list(2.5, minp.z, CAVESIZE * 10, CAVESIZE)
+	local rs = 1/SIZE
 
 	for x=minp2.x,maxp.x do
-		local cave1 = cave1x[x]
-		local cave2 = cave2x[x]
-		local land_base = heightx[x]
+		local cave1 = c1listx[x]
+		local cave2 = c2listx[x]
+		local land_base = hlistx[x]
 		for z=minp.z,maxp.z do
-			local cave1 = cave1+cave1z[z]
-			local cave2 = cave2+cave2z[z]
-			local land_base = land_base + heightz[z]
-			land_base = land_base + 1/10*math.sin(get_distance(x/SIZE,z/SIZE))
-			land_base = math.floor(SIZE*land_base)
-			local beach = math.floor(SIZE/97*math.cos((x - z)*10/(SIZE))) -- Also used for ice
+			local cave1 = cave1 + c1listz[z]
+			local cave2 = cave2 + c2listz[z]
+			local land_base = math.floor(SIZE * (
+				land_base
+				+ hlistz[z]
+				+ math.sin(get_distance(x*rs,z*rs)) * 0.1
+			))
+			local beach = math.floor(SIZE/97*math.cos((x - z)*10*rs)) -- Also used for ice
 			local lower_ground, cave_in_ended
 			for y=maxp.y,minp.y,-1 do
 				local p_pos = area:index(x, y, z)
@@ -141,8 +148,8 @@ table.insert(minetest.registered_on_generateds, 1, function(minp, maxp, seed)
 				and y > land_base then
 					data[p_pos] = c_water
 				else
-					local cave1 = (CAVESIZE/4 * (cave1+cave1y[y]))%2-1
-					local cave2 = (CAVESIZE/4 * (cave2+cave2y[y]))%2-1
+					local cave1 = (CAVESIZE*0.25 * (cave1 + c1listy[y]))%2-1
+					local cave2 = (CAVESIZE*0.25 * (cave2 + c2listy[y]))%2-1
 					cave = (cave1 < 0.5 and cave1 > -0.5) and (cave2 < 0.5 and cave2 > -0.5)
 					if not cave then
 						if y < land_base - 1 then
@@ -170,9 +177,7 @@ table.insert(minetest.registered_on_generateds, 1, function(minp, maxp, seed)
 								data[p_pos] = c_sandstone
 							end
 						elseif y == land_base + 1 then
-							local x = x * math.pi/12
-							local z = z * math.pi/12
-							local grid = 2*(math.sin(x) + math.sin(z))
+							local grid = 2*(math.sin(x * math.pi/12) + math.sin(z * math.pi/12)) --math.floor(2*(math.sin(x * 0.2) + math.sin(z * 0.2))*100+0.5)*0.01
 							if y < beach + h.ice
 							and y > beach + h.sea then
 								if grid == 4 then
